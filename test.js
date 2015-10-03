4ab2a2025735913ce79a6b0e48b4698941f2d3b0
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 17:18:28 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,29 +1,28 @@
-'use strict';
-var test = require('ava');
-var booleanify = require('./');
+import test from 'ava';
+import booleanify from './';
 
-test('should convert falsy values to `false`', function (t) {
-	[undefined, null, 0, '', NaN, false].forEach(function (val) {
+test('should convert falsy values to `false`', t => {
+	[undefined, null, 0, '', NaN, false].forEach(val => {
 		t.false(booleanify(val));
 	});
 
 	t.end();
 });
 
-test('should convert truthy values to `true`', function (t) {
-	['foobar', 20, true].forEach(function (val) {
+test('should convert truthy values to `true`', t => {
+	['foobar', 20, true].forEach(val => {
 		t.true(booleanify(val));
 	});
 
 	t.end();
 });
 
-test('should recognize \'true\'', function (t) {
+test('should recognize \'true\'', t => {
 	t.true(booleanify('true'));
 	t.end();
 });
 
-test('should recognize \'false\'', function (t) {
+test('should recognize \'false\'', t => {
 	t.false(booleanify('false'));
 	t.end();
 });
